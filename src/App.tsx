1a22b5e2e87ec4274787925ce2a4691bdbfc10a4
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2023-12-03 20:33:25 +0530

    Added utility class demonstration for tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          {/*Inline way of defining styles*/}
+          <div style={{backgroundColor: "red",
+              padding: 10, textAlign: "center"}}>
+              Inline CSS Box</div>
+          <br/>
+          {/*Using external CSS class of defining styles*/}
+          <div className="box">External CSS Box</div>
+          <br/>
+          {/*This is what tailwind CSS uses kind of atomic utility classes*/}
+          <div className="bg-red p-10 text-center">Tailwind CSS Utility Class Box</div>
+      </>
   );
 }
 
