e1a923ec253cbdc7406133a093956dd4c9dd16de
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2023-12-10 11:53:47 +0530

    Added height & width from screen size

--- src/MainContent/MainContent.tsx
+++ src/MainContent/MainContent.tsx
@@ -7,7 +7,7 @@ export class MainContent extends Component {
                 <h1 className="text-4xl text-center text-tertiary">
                     This is Main Content!</h1>
 
-                <div className="h-20 w-20 text-center p-1 bg-yellow-400">
+                <div className="h-20 w-1/3 text-center p-1 bg-yellow-400">
                     Height <br/>& <br/> Width
                 </div>
             </div>
