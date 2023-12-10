bef3c3aca0a6c3f703de5e016cdb4000398a8a30
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2023-12-10 14:29:07 +0530

    Added nav items

--- src/view/Navbar/Navbar.tsx
+++ src/view/Navbar/Navbar.tsx
@@ -4,11 +4,20 @@ import logo from '../../images/icon.png'
 export class Navbar extends Component {
     render() {
         return (
-            <div className="p-2 bg-[#444544] flex">
-                <h1 className="text-1xl text-secondary">
-                    Organic Shop</h1>
-                <img className="h-5 w-5 ml-1 pt-1" src={logo}
-                     alt=""/>
+            <div className="p-2 bg-[#444544] flex justify-between">
+
+                <div className="flex">
+                    <h1 className="text-1xl text-secondary">
+                        Organic Shop</h1>
+                    <img className="h-5 w-5 ml-1 pt-1" src={logo}
+                         alt=""/>
+                </div>
+
+                <ul className="list-none flex">
+                    <li className="mr-2 text-[11px] text-[#e6f0e6]">Home</li>
+                    <li className="mr-2 text-[11px] text-[#e6f0e6]">Contact</li>
+                    <li className="mr-2 text-[11px] text-[#e6f0e6]">About</li>
+                </ul>
             </div>
         );
     }
