b0b5e4e928985fd5763a16b308504452aacecef3
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2023-12-10 11:26:22 +0530

    Defined Navbar and Main Content

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import './App.css';
+import {Navbar} from "./Navbar/Navbar";
+import {MainContent} from "./MainContent/MainContent";
 
 function App() {
   return (
       <>
-          <div className="text-red-900 text-center">Using Pre-defined Tailwind CSS Utility classes as it is</div>
-          <br/>
-          <div className="text-red-300 text-center">Using Pre-defined Tailwind CSS Utility classes as it is</div>
-          <br/>
-          <div className="text-primary text-center">Using customized Tailwind utility CSS classes</div>
-          <br/>
-          <div className="text-secondary text-center">Using customized Tailwind utility CSS classes</div>
+          <Navbar/>
+          <MainContent/>
       </>
   );
 }
---
+++ src/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import {Component} from "react";
+
+export class Navbar extends Component {
+    render() {
+        return (
+            <div className="p-4 bg-blue-300">
+                <h1 className="text-4xl text-center text-tertiary">
+                    This is Navbar!</h1>
+            </div>
+        );
+    }
+}
