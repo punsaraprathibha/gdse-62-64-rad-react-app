365791022820a63264e8981109f47fbff13e4134
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2023-12-10 11:39:14 +0530

    Added gradient css

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import {Component} from "react";
 export class Navbar extends Component {
     render() {
         return (
-            <div className="p-4 bg-blue-300">
+            <div className="p-4 bg-blue-300 bg-gradient-to-r from-blue-900">
                 <h1 className="text-4xl text-center text-tertiary">
                     This is Navbar!</h1>
             </div>
