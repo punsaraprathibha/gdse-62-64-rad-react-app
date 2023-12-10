3404111040ab6b4dd0af6e3c42890ce2d1801cf1
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2023-12-10 11:44:38 +0530

    Added gradient to br css

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -4,9 +4,9 @@ export class Navbar extends Component {
     render() {
         return (
             <div className="p-4 bg-blue-300
-                            bg-gradient-to-r
-                            from-blue-900
-                            to-red-500">
+                            bg-gradient-to-br
+                            from-green-300
+                            to-yellow-300">
                 <h1 className="text-4xl text-center text-tertiary">
                     This is Navbar!</h1>
             </div>
