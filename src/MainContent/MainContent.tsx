c368ec136bfc42abbd1388452af9a1b9deaec473
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2023-12-10 12:02:47 +0530

    !important css added

--- src/MainContent/MainContent.tsx
+++ src/MainContent/MainContent.tsx
@@ -10,7 +10,7 @@ export class MainContent extends Component {
                 <div className="h-20 w-20 text-center p-1 bg-yellow-400
                                 rotate-12 shadow-gray-900 shadow-xl
                                 hover:shadow-red-500
-                                cursor-pointer">
+                                !cursor-pointer">
                     Height <br/>& <br/> Width
                 </div>
             </div>
