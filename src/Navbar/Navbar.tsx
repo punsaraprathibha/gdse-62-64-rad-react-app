0c2e84aa9ff0ce28fd0f4769b5e0de411247f554
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2023-12-10 13:15:03 +0530

    added via

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -6,6 +6,7 @@ export class Navbar extends Component {
             <div className="p-4 bg-blue-300
                             bg-gradient-to-br
                             from-green-300
+                            via-blue-700
                             to-yellow-300">
                 <h1 className="text-4xl text-center text-tertiary">
                     This is Navbar!</h1>
