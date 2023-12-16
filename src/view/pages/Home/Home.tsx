9dc37f3dcc3d8265a6844238170a61296589ed88
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2023-12-16 14:07:37 +0530

    home view completed

--- src/view/pages/Home/Home.tsx
+++ src/view/pages/Home/Home.tsx
@@ -1,6 +1,8 @@
 import {Component} from "react";
 
 import spinach from '../../../images/products/spinach.png';
+import tomato from '../../../images/products/tomato.png';
+import beans from '../../../images/products/beans.png';
 
 export class Home extends Component {
     render() {
@@ -39,32 +41,38 @@ export class Home extends Component {
                         </div>
                     </div>
 
-                    <div className="flex w-28 h-32
-                                    mr-2 mb-2 justify-center
-                                    items-center
-                                    border-gray-500 border-[0.5px]">
-                        02
+                    <div className="w-28 h-32 mr-2 mb-2 justify-center items-center border-gray-500 border-[0.5px]">
+                        <div>
+                            <img className="h-[88px] w-[88px]" src={tomato} alt=""/>
+                        </div>
+                        <div className="flex">
+                            <div>
+                                <h3 className="text-secondary text-[12px] pl-2">Tomato</h3>
+                            </div>
+                            <div className="bg-yellow-300 ml-1 p-[0.3px] rounded-lg pr-2">
+                                <h3 className="text-[12px] pl-2">300 <small className="text-[7px]">LKR</small></h3>
+                            </div>
+                        </div>
+                        <div className="flex justify-center">
+                            <button className="w-full mt-1 p-[2.4px] bg-secondary text-[8px] border-gray-500 border-[0.5px]">Add to Cart</button>
+                        </div>
                     </div>
 
-                    <div className="flex w-28 h-32
-                                    mr-2 mb-2 justify-center
-                                    items-center
-                                    border-gray-500 border-[0.5px]">
-                        03
-                    </div>
-
-                    <div className="flex w-28 h-32
-                                    mr-2 mb-2 justify-center
-                                    items-center
-                                    border-gray-500 border-[0.5px]">
-                        04
-                    </div>
-
-                    <div className="flex w-28 h-32
-                                    mr-2 mb-2 justify-center
-                                    items-center
-                                    border-gray-500 border-[0.5px]">
-                        05
+                    <div className="w-28 h-32 mr-2 mb-2 justify-center items-center border-gray-500 border-[0.5px]">
+                        <div>
+                            <img className="h-[88px] w-[88px]" src={beans} alt=""/>
+                        </div>
+                        <div className="flex">
+                            <div>
+                                <h3 className="text-secondary text-[12px] pl-2">Beans</h3>
+                            </div>
+                            <div className="bg-yellow-300 ml-1 p-[0.3px] rounded-lg pr-2">
+                                <h3 className="text-[12px] pl-2">250 <small className="text-[7px]">LKR</small></h3>
+                            </div>
+                        </div>
+                        <div className="flex justify-center">
+                            <button className="w-full mt-1 p-[2.4px] bg-secondary text-[8px] border-gray-500 border-[0.5px]">Add to Cart</button>
+                        </div>
                     </div>
                 </div>
             </div>
